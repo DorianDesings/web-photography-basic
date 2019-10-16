731733c0e75ab8a5f0930c83dce3885366f51fa5
DorianDesings <dorianoriginaldesings@gmail.com>
2019-10-17 00:21:13 +0200

    Login/register terminado

--- dev/js/login-register.js
+++ dev/js/login-register.js
@@ -6,15 +6,16 @@ const registerModal = document.getElementById('register-modal')
 const loginForm = document.getElementById('login-form')
 const registerForm = document.getElementById('register-form')
 
-if (topbar) {
+if (topbar)
     topbar.addEventListener('click', (e) => {
         if (e.target.parentElement.dataset.type != undefined) {
             if (e.target.parentElement.dataset.type == 'login') {
                 loginModal.classList.add('lightbox--show')
+            } else if (e.target.parentElement.dataset.type == 'register') {
+                registerModal.classList.add('lightbox--show')
             }
         }
     })
-}
 
 if (loginModal) {
     loginModal.addEventListener('click', (e) => {
@@ -24,8 +25,30 @@ if (loginModal) {
     })
 }
 
+if (registerModal) {
+    registerModal.addEventListener('click', (e) => {
+        if (e.target.classList.contains('lightbox')) {
+            registerModal.classList.remove('lightbox--show')
+        }
+    })
+}
+
 if (loginForm) {
     loginForm.addEventListener('submit', (e) => {
         e.preventDefault()
+        setTimeout(() => {
+            e.target.button.blur()
+        }, 200);
+        console.log('SEND');
+    })
+}
+
+if (registerForm) {
+    registerForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        setTimeout(() => {
+            e.target.button.blur()
+        }, 200);
+        console.log('SEND');
     })
 }
